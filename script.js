fcc633370d36070f4ef8a1278ead3b1c9e73f1a2
snapecode <smasnape@gmail.com>
2017-08-24 18:32:26 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+
+const MYCONSTANT = 5;
+console.log(MYCONSTANT);
+
+MYCONSTANT = 6;  
+
+*/
+
+
+function calculator(a, b, c){
+	a>0 && b>0 && c=="plus" ? 
+	console.log(a+b) : console.log (a-b);
+
+
+
+	
+}
+
